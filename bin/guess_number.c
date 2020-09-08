9c4c1896371432b08aa56d93c836adfc2a3ae50f
Davide Castellani <38205382+DavideC03@users.noreply.github.com>
2020-09-08 11:03:18 +0200

    Created bin/template.c

---
+++ bin/guess_number.c
@@ -0,0 +1,22 @@
+/**
+ * @file guess_number.cpp
+ *
+ * @version 01.01 2020-08-15
+ *
+ * @brief Guess number c
+ *
+ * @ingroup guess_number
+ * (Note: this needs exactly one @defgroup somewhere)
+ *
+ * @author Castellani Davide
+ *
+ * Contact: contacts@castellanidavide.it
+ *
+ */
+#include <stdio.h>
+
+int main() {
+	// printf() displays the string inside quotation
+	printf("guess_number");
+	return 0;
+}
